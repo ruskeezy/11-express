bf040344bf400014ac9046982dc09d5ac0a08ef2
Christian Miller <christian.ax.miller@gmail.com>
2018-02-26 21:04:48 -0800

    cat constructor added

---
+++ lab-christian/model/cat.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const uuidv4 = require('uuid/v4');
+// const createError = require('http-errors');
+const debug = require('debug')('note:note');
+const storage = require('../lib/storage');
+
+const Cat = module.exports = function(name, color) {
+  debug('Cat construtor');
+
+  if (!name) throw new Error('expected name');
+  if (!color) throw new Error('expected content');
+  this.id = uuidv4();
+  this.name = name;
+  this.content = color;
+};
+
+Cat.createNote = function(_cat) {
+  debug('createNote');
+  try {
+    let note = new Cat(_cat.name, _cat.content);
+    return storage.createItem('note', note);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+Cat.fetchNote = function(id) {
+  debug('fetchNote');
+  return storage.fetchItem('note', id);
+};
+
